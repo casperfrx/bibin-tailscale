63a91924eb391adf5e6d7fcfb4d10febee2f5fdb
Simon M <git@simon.marache.net>
2020-05-30 18:38:03 +0100

    Use the prefix everywhere

--- src/params.rs
+++ src/params.rs
@@ -42,26 +42,3 @@ impl<'a, 'r> FromRequest<'a, 'r> for IsPlaintextRequest {
         }
     }
 }
-
-/// Gets the Host header from the request.
-///
-/// The inner value of this `HostHeader` will be `None` if there was no Host header
-/// on the request.
-pub struct HostHeader<'a>(pub Option<&'a str>);
-
-impl<'a> Deref for HostHeader<'a> {
-    type Target = Option<&'a str>;
-
-    fn deref(&self) -> &Option<&'a str> {
-        &self.0
-    }
-}
-
-#[async_trait]
-impl<'a, 'r> FromRequest<'a, 'r> for HostHeader<'a> {
-    type Error = ();
-
-    async fn from_request(request: &'a Request<'r>) -> Outcome<HostHeader<'a>, ()> {
-        Outcome::Success(HostHeader(request.headers().get_one("Host")))
-    }
-}
